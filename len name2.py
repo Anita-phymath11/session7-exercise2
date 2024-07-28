1e121642b1fc2c36e1a36446af7a55ca1ade0dc2
Anita-phymath11 <a90k.karami@gmail.com>
2024-07-28 12:56:56 +0330

    Update len name2.py

--- len name2.py
+++ len name2.py
@@ -1,8 +1,17 @@
-a=[]
-u_name=input("enter your name please: ")
-a[1:2]=u_name
-print(a,)
 
+#what does this program do?
+# This program takes the name
+# from the user
+# and shows its characters 
+# as elements of a list.
+#//////////////////////////////////////////
+#solution 1:
+# a=[]
 # u_name=input("enter your name please: ")
-# lst=list(u_name)
-# print(lst)
+# a[1:2]=u_name
+# print(a,)
+#..........................................
+#solution 2:
+u_name=input("enter your name please: ")
+lst=list(u_name)
+print(lst)
